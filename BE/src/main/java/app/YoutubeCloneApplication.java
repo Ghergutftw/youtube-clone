0de628815a750a73f50f71388d6fa39193401bbf
GHERGUT MADALIN <madalinghergut@gmail.com>
2024-05-03 22:34:35 +0300

    dasda

--- BE/src/main/java/app/YoutubeCloneApplication.java
+++ BE/src/main/java/app/YoutubeCloneApplication.java
@@ -1,13 +1,13 @@
-package BE;
+package app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BeApplication {
+public class YoutubeCloneApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BeApplication.class, args);
+		SpringApplication.run(YoutubeCloneApplication.class, args);
 	}
 
 }
